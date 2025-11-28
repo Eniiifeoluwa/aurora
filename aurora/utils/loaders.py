09881857bb88bfd1df0b3afd683f3fb766d1cbb2
Akinola Samuel Afolabi <93071381+Eniiifeoluwa@users.noreply.github.com>
2025-11-28 19:40:42 +0100

    Update import path for document loaders

--- aurora/utils/loaders.py
+++ aurora/utils/loaders.py
@@ -1,4 +1,4 @@
-from langchain.document_loaders import TextLoader, PyPDFLoader
+from langchain_community.document_loaders import TextLoader, PyPDFLoader
 from typing import List
 from langchain.schema import Document
 import io
